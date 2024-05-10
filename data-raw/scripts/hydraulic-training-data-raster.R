1b4f19136a8ca2d2b0f0555ab4102165348bee10
skylerlewis <slewis@flowwest.com>
2024-05-10 09:53:05 -0700

    functionalizing raster suitability; test with Tuolumne Basso-La Grange model

---
+++ data-raw/scripts/hydraulic-training-data-raster.R
@@ -0,0 +1,113 @@
+library(tidyverse)
+library(sf)
+library(terra)
+
+source(here::here("data-raw", "scripts", "data-functions.R"))
+source(here::here("data-raw", "scripts", "suitability-functions.R"))
+
+flowlines <- readRDS(here::here("data-raw", "results", "flowline_geometries_proj.Rds"))
+
+# TUOLUMNE BASSO-LA GRANGE -----------------------------------------------------
+
+basso_dir <- here::here("data-raw", "temp", "tuolumne-basso")
+dir.create(basso_dir, recursive=T)
+drive_file_by_id("1nmczhbtWnCfmFaFPTol6rmRjnRh3ovCB") |>
+  archive::archive_extract(basso_dir)
+
+basso_filenames <-
+  tribble(~flow_cfs, ~depth, ~velocity,
+          80, "Depth (02JAN1900 00 00 00).vrt", "Velocity (02JAN1900 00 00 00).vrt",
+          110, "Depth (02JAN1900 16 00 00).vrt", "Velocity (02JAN1900 16 00 00).vrt",
+          150, "Depth (03JAN1900 08 00 00).vrt", "Velocity (03JAN1900 08 00 00).vrt",
+          300, "Depth (03JAN1900 18 00 00).vrt", "Velocity (03JAN1900 18 00 00).vrt",
+          500, "Depth (04JAN1900 04 00 00).vrt", "Velocity (04JAN1900 04 00 00).vrt",
+          600, "Depth (04JAN1900 14 00 00).vrt", "Velocity (04JAN1900 14 00 00).vrt",
+          900, "Depth (05JAN1900 00 00 00).vrt", "Velocity (05JAN1900 00 00 00).vrt",
+          1100, "Depth (05JAN1900 10 00 00).vrt", "Velocity (05JAN1900 10 00 00).vrt",
+          1450, "Depth (05JAN1900 20 00 00).vrt", "Velocity (05JAN1900 20 00 00).vrt",
+          3000, "Depth (06JAN1900 06 00 00).vrt", "Velocity (06JAN1900 06 00 00).vrt",
+          3200, "Depth (06JAN1900 16 00 00).vrt", "Velocity (06JAN1900 16 00 00).vrt",
+          4500, "Depth (07JAN1900 02 00 00).vrt", "Velocity (07JAN1900 02 00 00).vrt",
+          6000, "Depth (07JAN1900 12 00 00).vrt", "Velocity (07JAN1900 12 00 00).vrt",
+          7050, "Depth (07JAN1900 22 00 00).vrt", "Velocity (07JAN1900 22 00 00).vrt",
+          7600, "Depth (08JAN1900 08 00 00).vrt", "Velocity (08JAN1900 08 00 00).vrt",
+          9600, "Depth (08JAN1900 18 00 00).vrt", "Velocity (08JAN1900 18 00 00).vrt") |>
+  mutate(across(c(depth, velocity), function(x) file.path(basso_dir, x)))
+
+basso_groups <-
+  drive_file_by_id("1f_9Z8O64_t6mqicBW96KRBJ5ffOHqwen", vsizip=T) |>
+  st_read(as_tibble=T)
+
+basso_rast <- basso_filenames |>
+  prep_rast(.id_var = comid)
+
+outpath <- here::here("data-raw", "results", "fsa_basso.Rds")
+
+if(!file.exists(outpath)) {
+
+  basso_result <- basso_rast |>
+    raster_hsi(basso_groups, .id_var = comid) |>
+    post_rast(basso_groups, .id_var = comid)
+
+  basso_result |> saveRDS(outpath)
+
+} else {
+
+  basso_result <- readRDS(outpath)
+
+}
+
+# DEER CREEK -------------------------------------------------------------------
+
+deer_dir <- here::here("data-raw", "temp", "deer-creek")
+dir.create(deer_dir, recursive=T)
+drive_file_by_id("1rmMw6PXJGS0-ui52eaotABCSlJsZOzvr") |>
+  archive::archive_extract(deer_dir)
+
+deer_filenames <-
+  tribble(~flow_cfs, ~timestep,
+          100, "15NOV2018 06 00 00",
+          250, "15NOV2018 16 00 00",
+          300, "16NOV2018 02 00 00",
+          400, "16NOV2018 12 00 00",
+          500, "16NOV2018 22 00 00",
+          600, "17NOV2018 08 00 00",
+          1000, "17NOV2018 18 00 00",
+          3000, "18NOV2018 04 00 00",
+          5000, "18NOV2018 14 00 00",
+          6000, "19NOV2018 00 00 00",
+          7000, "19NOV2018 10 00 00",
+          9000, "19NOV2018 20 00 00",
+          10000, "20NOV2018 06 00 00",
+          11000, "20NOV2018 16 00 00",
+          12000, "21NOV2018 02 00 00",
+          13000, "21NOV2018 12 00 00",
+          14000, "21NOV2018 22 00 00",
+          15000, "22NOV2018 08 00 00") |>
+  mutate(depth = file.path(deer_dir, glue::glue("Depth ({timestep}).vrt")),
+         velocity = file.path(deer_dir, glue::glue("Velocity ({timestep}).vrt")))
+
+deer_groups <-
+  drive_file_by_id("1abatoUypK-oigD6zS6fUI7qmvEewZsSg", vsizip=T) |>
+  st_read(as_tibble=T) |>
+  janitor::clean_names() |>
+  filter(comid > 0)
+
+deer_rast <- basso_filenames |>
+  prep_rast(.id_var = comid)
+
+outpath <- here::here("data-raw", "results", "fsa_deer.Rds")
+
+if(!file.exists(outpath)) {
+
+  deer_result <- deer_rast |>
+    raster_hsi(deer_groups, .id_var = comid, parallel = TRUE) |>
+    post_rast(deer_groups, .id_var = comid)
+
+  deer_result |> saveRDS(outpath)
+
+} else {
+
+  deer_result <- readRDS(outpath)
+
+}
--- data-raw/scripts/suitability-functions.R
+++ data-raw/scripts/suitability-functions.R
@@ -1,5 +1,14 @@
+library(tidyverse)
+library(sf)
+library(terra)
+
+library(future) # parallel processing backend
+library(future.apply) # parallelized versions of lapply, sapply, ...
+library(furrr) # parallelized versions of purrr functions: map, map2, pmap,...
+
 ################################################################################
 # VECTOR MESH FUNCTIONS
+# Use for native SRH-2D model outputs
 ################################################################################
 
 # Prep Mesh --------------------------------------------------------------------
@@ -70,11 +79,12 @@ summarize_mesh <- function(mesh, group_var = NULL) {
 
 ################################################################################
 # RASTER FUNCTIONS
+# Use for native HEC-RAS 2D model outputs
 ################################################################################
 
 # Prep Flow Raster Stacks ------------------------------------------------------
 
-# Input: a named list of raster filenames, named by flow
+# Input: a data frame of raster filenames with columns flow_cfs, depth, velocity
 prep_rast <- function(filenames){
 
   dep_r <- terra::rast(filenames$depth)
@@ -83,91 +93,163 @@ prep_rast <- function(filenames){
   vel_r <- terra::rast(filenames$velocity)
   terra::set.names(vel_r, filenames$flow_cfs)
 
-  #return(list("depth" = terra::wrap(dep_r),
-  #            "velocity" = terra::wrap(vel_r)))
-
-  return(list("depth" = dep_r, #terra::wrap(dep_r),
-              "velocity" = vel_r))#,terra::wrap(vel_r)))
+  return(list("depth" = dep_r,
+              "velocity" = vel_r))
 
 }
 
-# HSI Function -----------------------------------------------------------------
-dvhsi <- function(d, v) (d>0.5 & d<=5.2) & (v>0 & v<=4.0)
-# ------------------------------------------------------------------------------
+# Depth/velocity HSI functions to be selected from -----------------------------
 
+dvhsi_hqt <- function(d, v) (d>1.0 & d<=3.28) & (v>0 & v<=1.5)
+dvhsi_lyr <- function(d, v) (d>0.5 & d<=5.2) & (v>0 & v<=4.0)
+
+# Main HSI Calculation Function ------------------------------------------------
+# input should be the output from prep_rast()
 raster_hsi <- function(rasters,
-                       group_polygons,
-                       mask_raster,
-                       hsi_func){
+                       group_polygons = NULL,
+                       #mask_raster = NULL,
+                       hsi_func = dvhsi_hqt,
+                       .id_var = comid,
+                       parallel = FALSE){
 
-  if(!missing(group_polygons)){
+  if(!is.null(group_polygons)){
     grp <- terra::vect(group_polygons)
   }
 
-  #dep <- terra::unwrap(rasters$depth)
-  #vel <- terra::unwrap(rasters$velocity)
-
   cell_area_ft2 <- prod(terra::res(rasters$depth)) * (terra::linearUnits(rasters$depth)/0.3048)^2
 
+  if (parallel) {
+
+    dep <- terra::wrap(rasters$depth)
+    vel <- terra::wrap(rasters$velocity)
+
+    if (future::availableCores()>1) {
+        future::plan(future::multisession, workers = future::availableCores())
+    } else {
+        future::plan(future::sequential)
+    }
+
+  }
+
   run_for_flow <- function(flow) {
-    d <- rasters$depth[[as.character(flow)]]
-    v <- rasters$velocity[[as.character(flow)]]
+
+    if (parallel) {
+      d <- terra::unwrap(dep)[[as.character(flow)]]
+      v <- terra::unwrap(vel)[[as.character(flow)]]
+    } else {
+      d <- rasters$depth[[as.character(flow)]]
+      v <- rasters$velocity[[as.character(flow)]]
+    }
 
     message(paste(flow, " - calculate inundation extent"))
     ext <- (d > 0)
-    message(paste(flow, " - apply habitat suitability function"))
-    hsi <- hsi_func(d, v) * mask_raster
 
-    if(!missing(group_polygons)){
-      message(paste(flow, " - summarize inundation extent"))
+    message(paste(flow, " - apply habitat suitability function"))
+    hsi <- hsi_func(d, v) # * mask_raster
+
+    if(!is.null(group_polygons)){
+
+      message(paste(flow, " - summarize inundation extent by group"))
       area_tot <- terra::zonal(ext, grp, "sum", na.rm=TRUE)[[1]] * cell_area_ft2
-      message(paste(flow, " - summarize suitable habitat area"))
+
+      message(paste(flow, " - summarize suitable habitat area by group"))
       area_wua <- terra::zonal(hsi, grp, "sum", na.rm=TRUE)[[1]] * cell_area_ft2
+
+      return(tibble({{.id_var}} := group_polygons |> pull({{.id_var}}),
+                    area_tot,
+                    area_wua,
+                    area_pct = area_wua / area_tot))
+
     } else {
+
       message(paste(flow, " - summarize inundation extent"))
       area_tot <- terra::global(ext, "sum", na.rm=TRUE)[[1]] * cell_area_ft2
+
       message(paste(flow, " - summarize suitable habitat area"))
       area_wua <- terra::global(hsi, "sum", na.rm=TRUE)[[1]] * cell_area_ft2
+
+      return(list(area_tot = area_tot,
+                  area_wua = area_wua,
+                  area_pct = area_wua / area_tot))
+
     }
-
-    area_pct <- area_wua / area_tot
-
-    return(list("area_tot"=area_tot,
-                "area_wua"=area_wua,
-                "area_pct"=area_pct))
   }
 
-  result <- names(rasters$depth) |> lapply(run_for_flow) |> bind_rows()
+  if (parallel) {
+
+    result <- names(rasters$depth) |>
+      setNames(names(rasters$depth)) |>
+      future.apply::future_lapply(run_for_flow, future.seed = 47) |>
+      bind_rows(.id = "flow_cfs")
+
+  } else {
+
+    result <- names(rasters$depth) |>
+      setNames(names(rasters$depth)) |>
+      lapply(run_for_flow) |>
+      bind_rows(.id = "flow_cfs")
+  }
 
   return(result)
 
 }
 
+# Prep Flowlines Helper Function -----------------------------------------------
+# Extract NHD flowlines that correspond to polys and calculate length
 
+prep_flowlines <- function(group_polygons, .id_var = comid) {
 
+  flowlines <- readRDS(here::here("data-raw", "results", "flowline_geometries_proj.Rds"))
+  # TODO: Update this reference to flowline data to use the packaged flowline data
 
+  flowlines |>
+    filter(comid %in% (group_polygons |> pull({{.id_var}}))) |>
+    st_transform(st_crs(group_polygons)) |>
+    st_intersection(group_polygons |>
+                      summarize() |>
+                      st_union(by_feature = F)) |>
+    mutate(length_ft = st_length(geometry) |>
+             units::set_units("ft") |> units::drop_units())
+}
 
+# Postprocess raster, adding reach lengths -------------------------------------
 
+post_rast <- function(result_tbl, group_polygons, .id_var = comid) {
 
+  flowlines_prepped <- group_polygons |> prep_flowlines(.id_var = {{.id_var}})
 
+  result_tbl |>
+    left_join(st_drop_geometry(flowlines_prepped),
+            by=join_by({{.id_var}}),
+            relationship="many-to-one") |>
+    mutate(ind_per_lf = area_tot / length_ft,
+           wua_per_lf = area_wua / length_ft) |>
+    arrange(comid, flow_cfs)
 
+}
 
+# First Inundating Flow Raster Function ----------------------------------------
 
+# input should be the output from prep_rast()
+raster_first_inundating_flow <- function(rasters) {
 
+  rst <- rasters[[1]]
 
+  get_inundation_bnd <- function(q) {
+    message(q, " - get inundation extent")
+    (terra::ifel(rst[[q]] > 0, 1, NA) * as.numeric(q))
+  }
 
+  inundation_bnds <-
+    names(rst) |>
+    lapply(get_inundation_bnd) |>
+    terra::rast()
 
+  message("calculate first inundating flow raster")
+  first_inundating_flow <- inundation_bnds |>
+    terra::app(fun = "min", na.rm=TRUE)
+  terra::set.names(first_inundating_flow, "flow_cfs")
 
+  return(first_inundating_flow)
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
